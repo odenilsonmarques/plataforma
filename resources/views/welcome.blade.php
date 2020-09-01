57683fdb96bae38c05e33006a4ee7a39542b5f67
odenilson marques <odmarquesgm@gmil.com>
2020-09-01 12:02:53 -0300

    novas alteracoes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -492,78 +492,93 @@
 
 
     <!--------------------------------------------------------------------------->
-    <div id="myCarousepraias" class="carousel slide" data-ride="carousel" >
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-          <li data-target="#myCarousepraias" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousepraias" data-slide-to="1"></li>
-      </ol>
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-          <div class="item active">
-
+    <div class="container-fluid" style="color:#FFF;border:none;background-color:#64a0d4" id="praias">   
+      <div class="container"> 
+      <h2 class="text-left" style="color:#FFF;font-family: Fantasy;">Praias</h2><br>
+        <div id="myCarousepraias" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+              <li data-target="#myCarousepraias" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousepraias" data-slide-to="1"></li>
+          </ol>
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <div class="col-md-4" >
+                <div class="">
+                  <a href="#" >
+                    <img src="{{('/assets/img/praia-panaquatira.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
+                  </a>
+                </div>
+                <div class="carousel-caption">
+                  <h3>Praia de Panaquantira</h3>
+                </div> 
+              </div>
+              <div class="col-md-4" >
+                <div class="">
+                  <a href="#" >
+                    <img src="{{('/assets/img/praia-panaquatira.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
+                  </a>
+                </div>
+                <div class="carousel-caption">
+                  <h3>Praia de Panaquantira</h3>
+                </div> 
+              </div>
+              <div class="col-md-4" >
+                <div class="">
+                  <a href="#" >
+                    <img src="{{('/assets/img/praia-panaquatira.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
+                  </a>
+                </div>
+                <div class="carousel-caption">
+                  <h3>Praia de Panaquantira</h3>
+                </div> 
+              </div>        
+            </div>
+            <div class="item ">
               <div class="col-md-4">
-                  <div class="">
-                      <a href="#" >
-                          <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
-                          <img src="{{('/assets/img/img6.jpg')}}"  alt="Lights" style="width:100%;height: 50%;margin-top: 2%" class="zoom">
-                      </a>
-                  </div>
+                <div class="">
+                  <a href="#" >
+                    <img src="{{('/assets/img/praia-panaquatira.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
+                  </a>
+                </div>
+                <div class="carousel-caption">
+                  <h3>Praia de Panaquantira</h3>
+                </div>   
               </div>
               <div class="col-md-4">
-                  <div class="">
-                      <a href="#">
-                          <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height:50%" class="zoom">
-                          <img src="{{('/assets/img/img6.jpg')}}"  alt="Lights" style="width:100%;height:50%; margin-top: 2%" class="zoom">
-                      </a>
-                  </div>
+                <div class="">
+                  <a href="#" >
+                    <img src="{{('/assets/img/praia-panaquatira.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
+                  </a>
+                </div>
+                <div class="carousel-caption">
+                  <h3>Praia de Panaquantira</h3>
+                </div>   
               </div>
               <div class="col-md-4">
-                  <div class="">
-                      <a href="#">
-                          <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
-                          <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height: 50%;margin-top: 2%" class="zoom">
-                      </a>
-                  </div>
+                <div class="">
+                  <a href="#" >
+                    <img src="{{('/assets/img/praia-panaquatira.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
+                  </a>
+                </div>
+                <div class="carousel-caption">
+                  <h3>Praia de Panaquantira</h3>
+                </div>   
               </div>
-          </div>    
-          <div class="item ">
-            <div class="col-md-4">
-                <div class="">
-                    <a href="#" >
-                        <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
-                        <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height: 50%;margin-top: 2%" class="zoom">
-                    </a>
-                </div>
             </div>
-            <div class="col-md-4">
-                <div class="">
-                    <a href="#">
-                        <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height:50%" class="zoom">
-                        <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height:50%; margin-top: 2%" class="zoom">
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="">
-                    <a href="#">
-                        <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height: 50%" class="zoom">
-                        <img src="{{('/assets/img/img6.jpg')}}" alt="Lights" style="width:100%;height: 50%;margin-top: 2%" class="zoom">
-                    </a>
-                </div>
-            </div>
-        </div>               
-      </div>
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousepraias" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left"></span>
-          <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousepraias" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right"></span>
-          <span class="sr-only">Next</span>
-      </a>
-  </div>
+            <a class="left carousel-control" href="#myCarousepraias" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousepraias" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div><br><br><br><br><br><br>
+    </div>
 
     <!--------------------------------------------------------------------------->
 
